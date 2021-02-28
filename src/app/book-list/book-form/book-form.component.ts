297101abffe9d2e9f6c4b76bef848f4b176c5e11
Julie J <jeannet.julie@gmail.com>
2021-02-28 14:44:12 +0100

    utiliser l'API Firebase Storage afin de permettre à l'utilisateur d'ajouter une photo du livre, de l'afficher dans  SingleBookComponent

--- src/app/book-list/book-form/book-form.component.ts
+++ src/app/book-list/book-form/book-form.component.ts
@@ -29,9 +29,13 @@ import { Router } from '@angular/router';
 export class BookFormComponent implements OnInit {
 
   bookForm!: FormGroup;
+  // et pour que le service d'upload fonctionne
+  fileIsUploading = false;
+  fileUrl!: string;
+  fileUploaded = false;
 
   constructor(private formBuilder: FormBuilder, private booksService: BooksService,
-              private router: Router) { }
+    private router: Router) { }
 
   ngOnInit() {
     this.initForm();
@@ -45,14 +49,46 @@ export class BookFormComponent implements OnInit {
     });
   }
 
-  onSaveBook() {
-    const title = this.bookForm.get('title')!.value;
-    const author = this.bookForm.get('author')!.value;
-    const synopsis = this.bookForm.get('synopsis')!.value;
-    const newBook = new Book(title, author);
-    newBook.synopsis = synopsis;
-    this.booksService.createNewBook(newBook);
-    this.router.navigate(['/books']);
-  }
+// méthode qui permettra de lier le  <input type="file">  (que vous créerez par la suite) à la méthode  onUploadFile()  :
+
+  detectFiles(event:any) {
+    this.onUploadFile(event.target.files[0]);
 }
 
+  // méthode qui déclenchera  uploadFile()  et qui en récupérera l'URL retourné :
+  onUploadFile(file: File) {
+    this.fileIsUploading = true;
+    this.booksService.uploadFile(file).then(
+      //
+      (url:any) => {
+        this.fileUrl = url;
+        this.fileIsUploading = false;
+        this.fileUploaded = true;
+      }
+    );
+}
+
+//   onSaveBook() {
+//     const title = this.bookForm.get('title')!.value;
+//     const author = this.bookForm.get('author')!.value;
+//     const synopsis = this.bookForm.get('synopsis')!.value;
+//     const newBook = new Book(title, author);
+//     newBook.synopsis = synopsis;
+//     this.booksService.createNewBook(newBook);
+//     this.router.navigate(['/books']);
+//   }
+// }
+
+onSaveBook() {
+  const title = this.bookForm.get('title')!.value;
+  const author = this.bookForm.get('author')!.value;
+  const synopsis = this.bookForm.get('synopsis')!.value;
+  const newBook = new Book(title, author);
+  newBook.synopsis = synopsis;
+  if(this.fileUrl && this.fileUrl !== '') {
+    newBook.photo = this.fileUrl;
+  }
+  this.booksService.createNewBook(newBook);
+  this.router.navigate(['/books']);
+}
+}
--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -12,11 +12,11 @@ export class BooksService {
 
   books: Book[] = [];
   booksSubject = new Subject<Book[]>();
-  bookEl:any;
+  bookEl: any;
 
   constructor() {
     this.getBooks();
-}
+  }
 
   emitBooks() {
     this.booksSubject.next(this.books);
@@ -26,52 +26,102 @@ export class BooksService {
   // La méthode  ref()  retourne une référence au node demandé de la base de données, et  set()  fonctionne plus ou moins comme  put()  pour le HTTP : il écrit et remplace les données au node donné.
   saveBooks() {
     firebase.database().ref('/books').set(this.books);
-}
+  }
 
-getBooks() {
-  firebase.database().ref('/books')
-    .on('value', (data: DataSnapshot) => {
+  getBooks() {
+    firebase.database().ref('/books')
+      .on('value', (data: DataSnapshot) => {
         this.books = data.val() ? data.val() : [];
         this.emitBooks();
       }
-    );
-}
+      );
+  }
 
-getSingleBook(id: number) {
-  return new Promise(
-    (resolve, reject) => {
-      firebase.database().ref('/books/' + id).once('value').then(
-        (data: DataSnapshot) => {
-          resolve(data.val());
-        }, (error) => {
-          reject(error);
+  getSingleBook(id: number) {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.database().ref('/books/' + id).once('value').then(
+          (data: DataSnapshot) => {
+            resolve(data.val());
+          }, (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+
+  createNewBook(newBook: Book) {
+    this.books.push(newBook);
+    this.saveBooks();
+    this.emitBooks();
+  }
+
+  // removeBook(book: Book) {
+  //   const bookIndexToRemove = this.books.findIndex(
+  //     (bookEl) => {
+  //       if(bookEl === book) {
+  //         return true;
+  //       }
+  //       // obligée de rajouter pour l'erreur "not all code paths return a value"?
+  //       else return bookEl;
+  //     }
+  //   );
+  //   this.books.splice(bookIndexToRemove, 1);
+  //   this.saveBooks();
+  //   this.emitBooks();
+  // }
+
+  removeBook(book: Book) {
+    if (book.photo) {
+      const storageRef = firebase.storage().refFromURL(book.photo);
+      storageRef.delete().then(
+        () => {
+          console.log('Photo removed!');
+        },
+        (error) => {
+          console.log('Could not remove photo! : ' + error);
         }
       );
     }
-  );
-}
-
-
-createNewBook(newBook: Book) {
-  this.books.push(newBook);
-  this.saveBooks();
-  this.emitBooks();
-}
-
-removeBook(book: Book) {
-  const bookIndexToRemove = this.books.findIndex(
-    (bookEl) => {
-      if(bookEl === book) {
-        return true;
+    const bookIndexToRemove = this.books.findIndex(
+      (bookEl) => {
+        if (bookEl === book) {
+          return true;
+        }
+        //   obligée de rajouter pour l'erreur "not all code paths return a value"?
+        else return bookEl;
       }
-      // obligée de rajouter pour l'erreur "not all code paths return a value"?
-      else return bookEl;
-    }
-  );
-  this.books.splice(bookIndexToRemove, 1);
-  this.saveBooks();
-  this.emitBooks();
-}
+    );
+    this.books.splice(bookIndexToRemove, 1);
+    this.saveBooks();
+    this.emitBooks();
+  }
+
+
+  // pour l'upload de photos
+  uploadFile(file: File) {
+    return new Promise(
+      (resolve, reject) => {
+        const almostUniqueFileName = Date.now().toString();
+        const upload = firebase.storage().ref()
+          .child('images/' + almostUniqueFileName + file.name).put(file);
+        upload.on(firebase.storage.TaskEvent.STATE_CHANGED,
+          () => {
+            console.log('Chargement…');
+          },
+          (error) => {
+            console.log('Erreur de chargement ! : ' + error);
+            reject();
+          },
+          () => {
+            resolve(upload.snapshot.ref.getDownloadURL());
+          }
+        );
+      }
+    );
+  }
 
 
 }
