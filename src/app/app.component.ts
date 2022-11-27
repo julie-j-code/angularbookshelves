0031bacfe2fd12c22fc8e2a5f262b06d0d78ed7c
Julie J <jeannet.julie@gmail.com>
2022-11-27 09:10:57 +0100

    Réactivation base de données et ajustements CSS de ce premier exercice OPC pour Angular avec upload d'image, creation et suppression de contenu et enregistrement via firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import firebase from 'firebase/app';
+// import * as firebase from 'firebase';  en haut du fichier, mettant à disposition la méthode  initializeApp() ) :
+import firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -11,21 +12,24 @@ export class AppComponent {
 
 
   constructor() {
-    const config = {
-      // apiKey: 'AIzaSyCwfa_fKNCVrDMR1E88S79mpQP-6qertew4',
-      // authDomain: 'bookshelves-3d570.firebaseapp.com',
-      // databaseURL: 'https://bookshelves-3d570.firebaseio.com',
-      // projectId: 'bookshelves-3d570',
-      // storageBucket: 'bookshelves-3d570.appspot.com',
-      // messagingSenderId: '6634573823'
-      apiKey: "AIzaSyC5KFSIBr2B0iYgNWkNIOZ1oLqV-bIYP4Q",
-      authDomain: "bookshelf-363ff.firebaseapp.com",
-      databaseURL: "https://bookshelf-363ff-default-rtdb.europe-west1.firebasedatabase.app",
-      projectId: "bookshelf-363ff",
-      storageBucket: "bookshelf-363ff.appspot.com",
-      messagingSenderId: "466923998798"
-    };
-    firebase.initializeApp(config);
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC5KFSIBr2B0iYgNWkNIOZ1oLqV-bIYP4Q",
+  authDomain: "bookshelf-363ff.firebaseapp.com",
+  databaseURL: "https://bookshelf-363ff-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "bookshelf-363ff",
+  storageBucket: "bookshelf-363ff.appspot.com",
+  messagingSenderId: "466923998798",
+  appId: "1:466923998798:web:e0927920881e00b467fd1f"
+}
+    firebase.initializeApp(firebaseConfig);
+
+
   }
 
 
